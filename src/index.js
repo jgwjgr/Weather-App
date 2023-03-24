0e99255fd02fa6ce24f6012d616f6f21b9abc418
jgwjgr <junishict21@gmail.com>
2023-03-24 16:27:33 +0100

    added function for searching city and show temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert(Hello);
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML =
+    Math.round(response.data.main.temp) + "°C";
+}
+
+function searchCity(city) {
+  let apiKey = "934d63b7a6b8ae0196e30b31c17ed43d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
