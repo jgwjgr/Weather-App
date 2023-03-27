378a0a9be195f7687ba70f3bb14c702a2ae7bcc7
jgwjgr <junishict21@gmail.com>
2023-03-27 23:06:16 +0200

    added forecast

--- src/index.js
+++ src/index.js
@@ -22,6 +22,41 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+                  <div class="col-2">
+                    <div class="weather-forecast-date">${day}</div>
+
+                    <img
+                      src="https://openweathermap.org/img/wn/10d@2x.png"
+                      alt=""
+                      width="44"
+                    />
+                    <div class="weather-forecast-temperature">
+                      <span class="weather-forecast-temperature-max">
+                        18°
+                      </span>
+                      <span class="weather-forecast-temperature-min">
+                        12°
+                      </span>&nbsp&nbsp&nbsp&nbsp&nbsp
+                    </div>
+                  
+                  </div>
+               
+              `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   let temperatureElement = document.querySelector("#temperature");
@@ -86,3 +121,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let celsiusTemperature = null;
+displayForecast();
